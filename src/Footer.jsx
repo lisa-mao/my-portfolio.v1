2a8d6ad436df4bd9937299aa3f014c4ccb0033ff
Lisa <lisamao2007@gmail.com>
2026-01-27 12:58:29 +0100

    basic sections made; introduction, myprojects and contact

---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+        <section className="bg-[#132a13] text-white">
+            <h1 id="footer">
+                Footer
+            </h1>
+        </section>
+    );
+}
+
+export default Footer
