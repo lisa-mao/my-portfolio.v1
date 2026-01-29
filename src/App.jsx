369fdc3c627bc675d5567dbb91c1a455581751a6
Lisa <lisamao2007@gmail.com>
2026-01-29 10:10:47 +0100

    deploy fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
-import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+import {BrowserRouter, Routes, Route, Link, HashRouter} from 'react-router-dom';
 import Intro from "./Intro.jsx";
-
-
 import AboutMe from "./AboutMe.jsx";
 import MyProjects from "./MyProjects.jsx";
 import Contact from "./Contact.jsx";
@@ -14,36 +12,36 @@ function App() {
     const closeModal = () => setActiveModal(null)
 
     return (
-
-        <main className="backgroundDay bg-cover bg-no-repeat h-screen w-screen">
-            <img src="/images/sun.png" alt="about me" className="size-40 object-top-right hover:cursor-pointer
+        <HashRouter>
+            <main className="backgroundDay bg-cover bg-no-repeat h-screen w-screen">
+                <img src="/images/sun.png" alt="about me" className="size-40 object-top-right hover:cursor-pointer
                  scale-50"/>
-            <section className="  flex justify-center items-center ">
+                <section className="  flex justify-center items-center ">
 
 
-                <div className="relative">
-                    <Intro onOpen={setActiveModal}/>
+                    <div className="relative">
+                        <Intro onOpen={setActiveModal}/>
 
-                    {activeModal === 'aboutMe' && (
-                        <AboutMe onClose={(closeModal)}/>
-                    )}
+                        {activeModal === 'aboutMe' && (
+                            <AboutMe onClose={(closeModal)}/>
+                        )}
 
-                    {activeModal === 'myProjects' && (
-                        <MyProjects onClose={(closeModal)}/>
-                    )}
+                        {activeModal === 'myProjects' && (
+                            <MyProjects onClose={(closeModal)}/>
+                        )}
 
-                    {activeModal === 'contact' && (
-                        <Contact onClose={(closeModal)}/>
-                    )}
-
-
-                </div>
+                        {activeModal === 'contact' && (
+                            <Contact onClose={(closeModal)}/>
+                        )}
 
 
-            </section>
-            {/*<Footer/>*/}
-        </main>
+                    </div>
 
+
+                </section>
+                {/*<Footer/>*/}
+            </main>
+        </HashRouter>
     )
 }
 
